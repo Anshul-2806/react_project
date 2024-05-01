2513e3c41920dd0ef2f404dee85f2cbcbc716e1f
Anshul-2806 <guptaanshul2806@gmail.com>
2024-05-01 13:57:38 +0530

    hehe anshul

--- src/myComponents/TodoItem.js
+++ src/myComponents/TodoItem.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export default function TodoItem(todo, onDelete) {
+export default function TodoItem(props) {
+  const onDelete = () => {}
   return (
     <div>
       <h4>{props.todo.title}</h4>
       <p>{props.todo.desc}</p>
-      <button className="btn btn-sm btn-danger" onClick={onDelete
-      }>Delete</button>
+      <button className="btn btn-sm btn-danger" onClick={onDelete}>Delete</button>
     </div>
   )
 }
